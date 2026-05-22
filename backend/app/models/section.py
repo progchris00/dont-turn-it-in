adafe16d789d68626669ef9ba90bf3ce150a8c34
Christian <prog.christian07@gmail.com>
2026-05-23 06:22:32 +0800

    chore: delete everything

--- backend/app/models/section.py
+++
@@ -1,8 +0,0 @@
-from typing import Optional
-
-from sqlmodel import Field, SQLModel
-
-
-class Section(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    name: str
--- docs/setup.md
+++
@@ -1,7 +0,0 @@
-# Setup ML
-```bash
-cd backend
-python -m venv .venv
-source .venv/bin/activate
-pip install -r requirements.txt
-```
